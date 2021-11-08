e9a269479a238bc3a8521f4e126306a497706295
BrianThePony <59828664+BrianThePony@users.noreply.github.com>
2021-11-08 16:14:43 +0100

    train and test split 80:20

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ num_classes = 2
 num_infeatures = 10 #INSERT
 
 # Load Datasets
-from loadData import loadData
+import loadData
+data = loadData.Dataset('',None)
+
+
+
 
 
 # Split datasets into train/test
@@ -21,6 +25,27 @@ import random
 import numpy as np
 random.seed(1337)
 np.random.seed(1337)
+
+noIMG = 2177
+train_IMG = []
+train_Labels = [] 
+test_IMG = []
+test_Labels = []
+
+a = list(range(2177))
+b = (random.sample(a,int(len(a)*0.8)))
+c = list(set(a) - set(b))
+
+for i in b:
+    print(i)
+    train_IMG.append(data.__getitem__(i))
+    train_Labels.append(data.__getitem__(i))
+for i in c:
+    print(i)
+    test_IMG.append(data.__getitem__(i))
+    test_Labels.append(data.__getitem__(i))
+
+
 
 # Get network
 
