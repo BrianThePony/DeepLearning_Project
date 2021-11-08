7cd4f1f3f4fce379b754aaf72f4a3f9efac30099
Niko1241 <31701426+Niko1241@users.noreply.github.com>
2021-11-08 14:54:55 +0100

    Much stuff

--- loadData.py
+++ loadData.py
@@ -1,3 +1,87 @@
 def loadData(filename):
     import numpy as np
     datasource = open(filename)
+    
+import os
+import numpy as np
+import torch
+import cv2
+from PIL import Image
+import re
+
+
+class PennFudanDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.frames = list(sorted(os.listdir(os.path.join(root, "project_20_data/video1/frames"))))
+        self.img = list(sorted(os.listdir(os.path.join(root, "project_20_data/video1/imgs"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "project_20_data/video1/frames", self.imgs[idx])
+        mask_path = os.path.join(self.root, "project_20_data/video1/frames", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        #mask = Image.open(mask_path)
+        # convert the PIL Image into a numpy array
+        #mask = np.array(mask)
+        # instances are encoded as different colors
+        #obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        #obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        # masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        #num_objs = len(obj_ids)
+        text = open(mask_path).read()
+        num_objs = len([m.start() for m in re.finditer("<xmin>",text)])
+        xminindices = [m.start() for m in re.finditer("<xmin>",text)]+6
+        xminendindices = [m.start() for m in re.finditer("</xmin>",text)]-1
+        xmaxindices = [m.start() for m in re.finditer("<xmax>",text)]+6
+        xmaxendindices = [m.start() for m in re.finditer("</xmax>",text)]-1
+        yminindices = [m.start() for m in re.finditer("<ymin>",text)]+6
+        yminendindices = [m.start() for m in re.finditer("</ymin>",text)]-1
+        ymaxindices = [m.start() for m in re.finditer("<ymax>",text)]+6
+        ymaxendindices = [m.start() for m in re.finditer("</ymax>",text)]-1
+        boxes = []
+        for i in range(num_objs):
+            xmin = float(text[xminindices[i]:xminendindices[i]])
+            xmax = float(text[xmaxindices[i]:xmaxendindices[i]])
+            ymin = float(text[yminindices[i]:yminendindices[i]])
+            ymax = float(text[ymaxindices[i]:ymaxendindices[i]])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
