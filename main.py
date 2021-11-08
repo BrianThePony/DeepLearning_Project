4e855511de961c44e04b9a9f325d25c55b37cd38
BjarkeN <Bjarken98@gmail.com>
2021-11-08 15:11:11 +0100

    Added main template

--- main.py
+++ main.py
@@ -5,23 +5,44 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import network
+import utils
+from engine import train_one_epoch, evaluate
 
 
 
 # Hyper Parameters
 num_classes = 2
-num_infeatures = 10 #INSERT
 
 # Load Datasets
+dataset = PennFudanDataset()
+dataset_test = PennFudanDataset()
 
 # Split datasets into train/test
+indices = torch.randperm(len(dataset)).tolist()
+dataset_train = torch.utils.data.Subset(dataset, indices[:-50])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
 # Get network
-model = network.get_model()
+model = network.get_model(num_classes)
 
 # Define optimizer
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.005,
+                            momentum=0.9, weight_decay=0.0005)
+# Define learning rate scheduler
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                               step_size=3,
+                                               gamma=0.1)
 
 # Model training
-#output = model(images,targets)   # Returns losses and detections
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    # train for one epoch, printing every 10 iterations
+    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+    # update the learning rate
+    lr_scheduler.step()
+    # evaluate on the test dataset
+    evaluate(model, data_loader_test, device=device)
 
 # Model Show
