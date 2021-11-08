5a44d1e8877d5b9c6648efcc104d49507ae4807f
BrianThePony <59828664+BrianThePony@users.noreply.github.com>
2021-11-08 14:18:39 +0100

    pngbillederVideo1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import torchvision
-from torchvision.models.detection import FasterRCNN
+
 
 num_classes = 2
+
